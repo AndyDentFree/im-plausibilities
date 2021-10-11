629bcf20ed3c24aca08cb19ebcc016812310b4b4
Andy Dent <me@andydent.com>
2021-10-12 01:04:20 +0800

    imUrlDataAppSUI - Adding Basic UI layout ContentView.swift - filled in blank View with four buttons and switches plus static label, in stack views

--- imUrlDataAppSUI/imUrlDataAppSUI/ContentView.swift
+++ imUrlDataAppSUI/imUrlDataAppSUI/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var happyAllowed = true
+    @State var quizzicalAllowed = true
+    @State var distraughtAllowed = true
+    @State var angryAllowed = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center) {
+            Text("Choose moods allowed to send")
+            VStack(alignment: .center, spacing: 0) {
+                HStack() {
+                    Toggle("", isOn: $happyAllowed).labelsHidden()
+                    Text("😀")
+                }
+                HStack {
+                    Toggle("", isOn: $quizzicalAllowed).labelsHidden()
+                    Text("🤨")
+                }
+                HStack {
+                    Toggle("", isOn: $distraughtAllowed).labelsHidden()
+                    Text("😫")
+                }
+                HStack {
+                    Toggle("", isOn: $angryAllowed).labelsHidden()
+                    Text("😫")
+                }
+            }
+            .font(Font.system(size: 80.0))
+            Button("Send Message from App", action: {})
+        }
     }
 }
 
