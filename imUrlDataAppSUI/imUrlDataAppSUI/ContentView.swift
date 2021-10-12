791a74ad9ffd69d0a6b911ad354bccceefd5efa5
Andy Dent <me@andydent.com>
2021-10-12 11:08:23 +0800

    Adding Basic UI, not sharing data - added state variables with toggles so icon opacity is dependent

--- imUrlDataAppSUI/imUrlDataAppSUI/ContentView.swift
+++ imUrlDataAppSUI/imUrlDataAppSUI/ContentView.swift
@@ -7,30 +7,36 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    //TODO change this into a collection and use ForEach to build the VStack
     @State var happyAllowed = true
     @State var quizzicalAllowed = true
     @State var distraughtAllowed = true
     @State var angryAllowed = true
+    var happyOpacity: Double {happyAllowed ? 1.0 : 0.3}
+    var quizzicalOpacity: Double {quizzicalAllowed ? 1.0 : 0.3}
+    var distraughtOpacity: Double {distraughtAllowed ? 1.0 : 0.3}
+    var angryOpacity: Double {angryAllowed ? 1.0 : 0.3}
     var body: some View {
         VStack(alignment: .center) {
             Text("Choose moods allowed to send")
             VStack(alignment: .center, spacing: 0) {
                 HStack() {
                     Toggle("", isOn: $happyAllowed).labelsHidden()
-                    Text("😀")
+                    Text("😀").opacity((happyOpacity))
                 }
                 HStack {
                     Toggle("", isOn: $quizzicalAllowed).labelsHidden()
-                    Text("🤨")
+                    Text("🤨").opacity((quizzicalOpacity))
                 }
                 HStack {
                     Toggle("", isOn: $distraughtAllowed).labelsHidden()
-                    Text("😫")
+                    Text("😫").opacity((distraughtOpacity))
                 }
                 HStack {
                     Toggle("", isOn: $angryAllowed).labelsHidden()
-                    Text("😫")
+                    Text("😫").opacity((angryOpacity))
                 }
             }
             .font(Font.system(size: 80.0))
