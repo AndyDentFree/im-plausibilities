e740382d5598f869ba6a6ae0031b86cf68d7ece2
Andy Dent <me@andydent.com>
2022-06-09 21:31:20 +0800

    imUrlDataAppSUI - Adding Messenger extension Target showing SwiftUI
    imUrlDataAppSUI IM project target added as iMessage extension
    (comes in with Storyboard)
    
    Maininterface.storyboard
    - Messages View Controller
      - changed label text to be a big warning that SwiftUI is not hosted
    
    UIViewController+EmbedSwiftUI.swift
    - added to embed SwiftUI views
    
    ContentView.swift
    - add to imUrlDataSAppSUI IM project as well, just for now
    
    MessagesViewController.swift
    - viewDidLoad - embed an instance of ContentView

--- imUrlDataAppSUI/imUrlDataAppSUI IM/MessagesViewController.swift
+++ imUrlDataAppSUI/imUrlDataAppSUI IM/MessagesViewController.swift
@@ -13,6 +13,7 @@ class MessagesViewController: MSMessagesAppViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addSubSwiftUIView(ContentView(), to: view)
     }
     
     // MARK: - Conversation Handling
